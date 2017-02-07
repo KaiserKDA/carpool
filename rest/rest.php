ccff9695e4014f1bbd9322beec2e9ee5dbfd84c2
HorstVonMorgen <moritz-morgenstern@web.de>
2017-02-07 18:24:21 +0100

    Update rest.php

--- rest/rest.php
+++ rest/rest.php
@@ -1,19 +1,8 @@
 <?php
-$request_parts = explode('/', $_SERVER['REQUEST_URI']); // array('users', 'show', 'abc')
+$request_parts = explode('/', $_GET['param']); // array('users', 'show', 'abc')
 include('connect.php');
 
+print_r($request_parts);
 $output = get_data_from_db(); //Do your processing here
                               //You can outsource to other files via an include/require
-$file_type = $_GET['type'];
-//Output based on request
-switch($file_type) {
-    case 'json':
-        echo json_encode($output);
-        break;
-    case 'xml':
-        echo xml_encode($output); //This isn't a real function, but you can make one
-        break;
-    default:
-        echo $output;
-}
 ?>
