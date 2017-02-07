bb23a9665c5dc304cf79b4081484b1d878c33e28
HorstVonMorgen <moritz-morgenstern@web.de>
2017-02-07 18:29:09 +0100

    Update rest.php

--- rest/rest.php
+++ rest/rest.php
@@ -1,8 +1,16 @@
 <?php
-$request_parts = explode('/', $_GET['param']); // array('users', 'show', 'abc')
+try {
+	$request_parts = explode('/', $_GET['param']); // array('users', 'show', 'abc')
 include('connect.php');
 
 print_r($request_parts);
 $output = get_data_from_db(); //Do your processing here
                               //You can outsource to other files via an include/require
+
+
+
+} catch (Exception $e) {
+    echo 'Exception: ',  $e->getMessage(), "\n";
+    header("HTTP/1.0 404 Not Found");
+}
 ?>
